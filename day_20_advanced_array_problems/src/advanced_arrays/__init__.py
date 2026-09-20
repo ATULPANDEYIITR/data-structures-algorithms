87257f86893f84a4a405f83ed0942b4d7158800f
ATUL PANDEY <atulpandey.iitr@gmail.com>
2026-09-20 23:58:38 +0530

    Create __init__.py for advanced array algorithms module
    This file initializes the advanced array algorithms module and specifies the public API.

---
+++ day_20_advanced_array_problems/src/advanced_arrays/__init__.py
@@ -0,0 +1,29 @@
+"""Advanced array algorithms and supporting utilities."""
+
+from .algorithms import (
+    apply_permutation,
+    find_duplicate,
+    find_majority_element,
+    find_missing_number,
+    longest_consecutive_sequence,
+    max_subarray_sum,
+    merge_intervals,
+    product_except_self,
+    rearrange_alternating,
+    rotate_array,
+    three_sum,
+)
+
+__all__ = [
+    "apply_permutation",
+    "find_duplicate",
+    "find_majority_element",
+    "find_missing_number",
+    "longest_consecutive_sequence",
+    "max_subarray_sum",
+    "merge_intervals",
+    "product_except_self",
+    "rearrange_alternating",
+    "rotate_array",
+    "three_sum",
+]
